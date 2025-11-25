dcfc5e3325de93dd23f015431a08487cca68043c
AmitSharma <Sandy31524@gmail.com>
2025-11-25 15:57:17 +0530

    11

--- DSA/Array.java
+++ DSA/Array.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Array{
     public static void main(String[] args) {
         
-        ArrayList<String> Food = new ArrayList<>();
+        ArrayList<String> Food = new ArrayList<String>();
 
         Food.add("Pizza");
         Food.add("burger");
