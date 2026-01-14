ac5dde7c7090e8b7e83cd34bbc9cfa1a13f00899
AmitSharma <Sandy31524@gmail.com>
2026-01-14 21:29:34 +0530

    create file

---
+++ Basics/Files.java
@@ -0,0 +1,19 @@
+package Basics;
+import java.io.File;
+
+public class Files {
+    public static void main(String[] args) {
+        try {
+            File file = new File("data.txt");
+            if(file.createNewFile()){
+                System.out.println("File Created");
+                file.canWrite();
+            } else {
+                System.out.println("File is not created");
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Error in file creating "+e.getMessage());
+        }
+    }
+}
