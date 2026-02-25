f0be153f9ed7b039cadba4601ad792363d13f957
AmitSharma <Sandy31524@gmail.com>
2026-02-25 18:53:15 +0530

    total sum

--- practice/hello.c
+++ practice/hello.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 
-int main() {
-    int age;
-    printf("Enter your age: ");
-    scanf("%d",&age);
+int addSum(int arr[],int k){
+    int sum  = 0;
+    for(int i = 0;i<k;i++){
+        sum += arr[i];
+        
+   }
+   return sum;
+}
 
-    if(age < 18) {
-        printf("your age must have 18+ to vote");
-    } else {
-        printf("you can vote");
+int main() {
+    int k;
+    printf("Enter the value of K: ");
+    scanf("%d",&k);
+    int arr[k];
+    for(int i = 0;i<k;i++){
+        printf("Enter the elements of arr %d: ",i+1);
+        scanf("%d",&arr[i]);
     }
+    int totalSum = addSum(arr,k);
+    printf("Total Sum: %d",totalSum);
 
     return 0;
-}
+
+}   
