6467e877949b59042799e5bedcf9d50b0ba284a4
AmitSharma <Sandy31524@gmail.com>
2026-03-25 08:19:11 +0530

    string buffer and string builder and their methods

--- Basics/Trial.java
+++ Basics/Trial.java
@@ -1,6 +1,16 @@
 public class Trial {
     public static void main(String[] args) {
-        char a = a;
-        System.out.println(a);
+        String string = "Naman";
+        StringBuffer name = new StringBuffer("Amit");
+        name.append("Sharma");
+        System.out.println(name.capacity());
+        System.out.println(name);
+        string = string + "Dubey";
+        System.out.println(string.hashCode());
+        System.out.println( "Hello "+string);
+        System.out.println(string.charAt(1) == string.charAt(3));
     }
 }
+
+
+
