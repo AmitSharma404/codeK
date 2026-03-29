0c778dcf7bac40bb63d2538dc61c7edd391e5aaa
AmitSharma <Sandy31524@gmail.com>
2026-03-29 21:25:13 +0530

    time taken in selection sort

--- Basics/Selection.java
+++ Basics/Selection.java
@@ -2,13 +2,10 @@ package Basics;
 import java.util.*;
 class Selection{
     public static void main(String[] args) {
-        long elapsedTime;
-        long startTime;
-        long endTime;
+        
 
-
-        int [] arr = {4,5,1,2,7,9,6};
-
+        int [] arr = {4,5,1,2,7,9,6,10,31,14,42,54,35,42,41,35,37};
+        long startTime = System.currentTimeMillis();
         for(int i= 0;i<arr.length-1;i++){
             for(int j = i+1;j<arr.length;j++){
                 if(arr[i] > arr[j]){
@@ -23,5 +20,11 @@ class Selection{
         for (int a = 0;a<arr.length;a++){
             System.out.print(arr[a] + " ");
         }
+
+        long endTime = System.currentTimeMillis();
+
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("totalTime: " + elapsedTime + "\tms");
     }
 }
