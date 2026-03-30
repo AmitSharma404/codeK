797dd04596525f3b92bbc0b0637b56f766252707
AmitSharma <Sandy31524@gmail.com>
2026-03-30 08:18:33 +0530

    binary search time taken in ms

--- DAA/BinarySearch.java
+++ DAA/BinarySearch.java
@@ -1,37 +1,46 @@
 package DAA;
+
 import java.util.Arrays;
+
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = new int[1000000];
-        int target = 900101;
-        for(int i = 0;i < arr.length;i++) {
-            arr[i] = i; 
-      }
+        int[] arr = new int[100000000];
+        int target = 32869134;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        long starttime = System.currentTimeMillis();
 
-        int element = Binarysearch(arr,target);
-
+        int element = Binarysearch(arr, target);
+        long endtime = System.currentTimeMillis();
+        long elapsedTime = endtime - starttime;
+        System.out.println("total time in ms: "+ elapsedTime + " ms");
         System.out.println("Target found at: " + element);
     }
 
-
-    private static int Binarysearch(int[] arr,int target) {
+    private static int Binarysearch(int[] arr, int target) {
+        long elapsedTime;
+        long starttime;
+        long endtime;
         int low = 0;
         int high = arr.length - 1;
-
+        
+        
         while (low <= high) {
             int mid = low + (high - low) / 2;
             int value = arr[mid];
             System.out.println("mid \t" + mid);
-            if(value < target) {
+            if (value < target) {
                 low = mid + 1;
-            } else if(value > target) {
+            } else if (value > target) {
                 high = mid - 1;
             } else {
                 return mid;
             }
         }
+    
         return 0;
+
     }
+
 }
-
-
