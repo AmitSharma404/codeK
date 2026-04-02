3e01c3eb14d235a4283ee7809ed6b1fd17e49cc5
AmitSharma <Sandy31524@gmail.com>
2026-04-02 21:45:53 +0530

    Animal class

---
+++ Basics/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    static String Color;
+    String name;
+    public Dog(String name){
+        System.out.println(this.name);
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        
+    }
+}
---
+++ Basics/DayOne.java
@@ -0,0 +1,23 @@
+
+
+
+
+public class DayOne {
+
+    public DayOne(){
+        System.out.println("Object is created");
+    }
+
+    public void show(){
+        System.out.println("In a A");
+    }
+
+    public static void main(String[] args) {
+
+        int marks = 10;
+        DayOne obj; 
+
+        new DayOne().show();
+    }
+
+}
