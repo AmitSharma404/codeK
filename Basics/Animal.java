a577f70d0916314867a065fb45176b9a10507113
AmitSharma <Sandy31524@gmail.com>
2026-04-03 23:55:41 +0530

    enew

--- Basics/Animal.java
+++ Basics/Animal.java
@@ -1,12 +1,13 @@
 public class Animal {
     static String Color;
     String name;
-    public Dog(String name){
+    public void Dog(String name){
+        this.name = "Bruno";
         System.out.println(this.name);
     }
 
     public static void main(String[] args) {
         Animal animal = new Animal();
-        
+        System.out.println(animal.name);
     }
 }
